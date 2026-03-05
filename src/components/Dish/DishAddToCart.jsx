57f1960170aeb6d33fd15e6941291cd723c70816
Antoine DC <antoinedeconto@gmail.com>
2026-03-05 21:06:24 +0100

    Ajout d'un MouseEnter

--- src/components/Dish/DishAddToCart.jsx
+++ src/components/Dish/DishAddToCart.jsx
@@ -10,6 +10,12 @@ export default function DishAddToCart({ dish }) {
           duration: 2000,
         });
       }}
+      onMouseEnter={(e) => {
+        e.preventDefault(); // Empêche la navigation lors du clic sur le bouton
+        toast.info(`Cliquez pour ajouter au panier !`, {
+          duration: 2000,
+        });
+      }}
       className="btnAddtoCart absolute bottom-4 right-4 bg-gradient-to-r from-purple-600 to-pink-600 text-white hover:text-black p-4 rounded-full shadow-xl hover:scale-110 transition-all transform cursor-pointer flex items-center justify-center opacity-0 group-hover:opacity-100"
       title="Ajouter au panier"
     >
