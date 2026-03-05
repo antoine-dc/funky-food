6e5f835222bec2ee25d53e939fe3780ec464369e
Antoine DC <antoinedeconto@gmail.com>
2026-03-05 16:25:07 +0100

    Ajout de style sur le Layout

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -1,7 +1,7 @@
 import { Outlet } from "react-router";
 import Footer from "./components/Footer";
 import Header from "./components/Header";
-import "./styles/App.css";
+import "./styles/Layout.css";
 
 function Layout() {
   return (
