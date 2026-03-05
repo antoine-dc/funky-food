2aff0f21a76883b0790c983323c96e50ee1e5625
Antoine DC <antoinedeconto@gmail.com>
2026-03-05 20:57:37 +0100

    Ajout du click avec un Toaster

--- src/components/Dish/DishAddToCart.jsx
+++ src/components/Dish/DishAddToCart.jsx
@@ -1,9 +1,15 @@
 import { ShoppingCart } from "lucide-react";
+import { toast } from "sonner";
 
-export default function DishAddToCart() {
+export default function DishAddToCart({ dish }) {
   return (
     <div
-      //   GERER LE CLICK ICI
+      onClick={(e) => {
+        e.preventDefault(); // Empêche la navigation lors du clic sur le bouton
+        toast.success(`${dish.name} ajouté au panier ! 🎉`, {
+          duration: 2000,
+        });
+      }}
       className="btnAddtoCart absolute bottom-4 right-4 bg-gradient-to-r from-purple-600 to-pink-600 text-white hover:text-black p-4 rounded-full shadow-xl hover:scale-110 transition-all transform cursor-pointer flex items-center justify-center opacity-0 group-hover:opacity-100"
       title="Ajouter au panier"
     >
